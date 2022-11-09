4f06a7955a9283e543b4a7bcece807f4f530ee32
Hyeonyeongg <96678570+Hyeonyeongg@users.noreply.github.com>
2022-11-09 20:12:38 +0900

    11월 9일

---
+++ React/017/모듈화_1/index.js
@@ -0,0 +1,5 @@
+import Main from './Main.js'
+//Main.js에서 변수일지 함수일지는 모르겠지만 Main이라는 애를 가져와라.
+
+//liveserver로 작동해야함.
+Main()
---
+++ React/017/모듈화_2/Main.js
@@ -0,0 +1,4 @@
+export default function (){
+    console.log('나 실행됐어!')
+}
+//밖으로 빼줄건데 얘를 기본으로 빼줘라.
