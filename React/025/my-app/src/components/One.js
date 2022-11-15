398723abf2f71e0d7c0589d18e59d6ebb8911869
Hyeonyeongg <96678570+Hyeonyeongg@users.noreply.github.com>
2022-11-15 20:11:55 +0900

    11월 15일

---
+++ React/025/my-app/src/components/One.js
@@ -0,0 +1,5 @@
+export default function One() {
+    return (
+        <div>One Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat voluptatem sint magnam quam quo quis sed aliquam natus odio dolor voluptatibus quaerat cupiditate eos architecto ipsam provident repellat, nam doloremque?</div>
+    )
+}
