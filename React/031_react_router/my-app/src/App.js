de88052a0a84a866cff01aa4bf878d4f34569248
Hyeonyeongg <96678570+Hyeonyeongg@users.noreply.github.com>
2022-11-17 12:47:55 +0900

    11월 17일

--- React/031_react_router/my-app/src/App.js
+++ React/031_react_router/my-app/src/App.js
@@ -1,73 +1,54 @@
-import { BrowserRouter, Routes, Route, Link, useLocation, Outlet } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link, useLocation, useParams, Outlet } from "react-router-dom";
+
+//문제
+// 문제
+// /
+// /a
+// /b
+// /c
+// /c/1
+// /c/2
+// /c/d
+
+
+function Home() {
+  return <div>홈</div>
+}
+
+function A() {
+  return <div>A</div>
+}
+
+function B() {
+  return <div>B</div>
+}
+
+function C() {
+  return <div>C</div>
+}
+
+function CDetail() {
+  return <div>디테일C</div>
+}
 
 function App() {
   return (
     <BrowserRouter>
-      <Link to="/"> home </Link>
-      <Link to="/one"> one </Link>
-      <Link to="/two"> two </Link>
-      <Link to="/three"> three </Link>
-      {/* 라우트를 감싸줍니다. */}
       <Routes>
-        <Route path="/" element={<Index />}/>
-        <Route path="/one" element={<One name='licat' />}/>
-        <Route path="/two" element={<Two />}/>
-        <Route path="/three/*" element={<Outlet />}>
-          {/* *은 뒤에 어떤 url이 오든 허용하겠다. 아래와 같은 것들을 처리할 때 사용함.  */}
-          {/* /a */}
-          {/* /a/b */}
-          {/* /a/b/c */}
-          {/* /a/b/c/10 */}
-          {/* /three/라고 그냥 들어올 경우. */}
-          <Route path="" element={<HojunIndex/>}/>
-          {/* /three/hojunone/으로 들어올 경우. path에 *에 자리에 들어가는 것을 명시해주는 것 같음. */}
-          <Route path="hojunone/" element={<HojunOne/>}/>
-          <Route path="hojuntwo/" element={<HojunTwo/>}/>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/a" element={<A/>}/>
+        <Route path="/b" element={<B/>}/>
+        {/* <Route path="/c" element={<C/>}/>
+        <Route path="/c/:id" element={<CDetail/>}/> */}
+        <Route path="/c/*" element={<Outlet/>}>
+          <Route path="" element={<C/>}/>
+          <Route path=":id" element={<CDetail/>}/>
         </Route>
-        <Route path="/blog/:id" element={<Blog />}/>
       </Routes>
     </BrowserRouter>
   );
 }
 
-function Index(){
-  return <h1>hello world0</h1>
-}
-
-function One({name}){
-  return <h1>{name} world1</h1>
-}
-
-function Two(){
-  return <h1>hello world2</h1>
-}
-
-function Three(){
-  return <h1>hello world3</h1>
-}
-
-function Blog(){
-    const location = useLocation();
-  console.log(location)
-  return <h1>hello Blog</h1>
-}
-
-function HojunIndex(){
-    const location = useLocation();
-  console.log(location)
-  return <h1>hello Hojun index</h1>
-}
-
-function HojunOne(){
-    const location = useLocation();
-  console.log(location)
-  return <h1>hello Hojun 1</h1>
-}
-
-function HojunTwo(){
-    const location = useLocation();
-  console.log(location)
-  return <h1>hello Hojun 2</h1>
-}
 
 export default App;
+
